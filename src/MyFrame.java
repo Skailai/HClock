d4a5ed25c940be1d33f90d7703a6949b3bc760e0
Skailai <102485824+Skailai@users.noreply.github.com>
2023-10-05 11:09:09 +0200

    Revert "version 1.0 of clock"
    This reverts commit a21a8a45df81cac96be8906bd1df0ecfcf818eda.

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -14,6 +14,7 @@ public class MyFrame extends JFrame{
         this.setResizable(false);
         this.setLayout(new FlowLayout());
 
+
         dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
 
         time = getTime();
